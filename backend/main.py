39f61e1dcf5fb84c2b6b1087e169550fb04d2caf
Gaston Oficina <genovesegaston@gmail.com>
2025-08-28 17:16:43 -0300

    "actualizacion"

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,9 @@ from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI(
-    
+    title= "Control de gastos API",
+    description="API para registrar y analizar gastos corrientes.",
+    version="0.1.0"
 )
 
 @app.get("/")
